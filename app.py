d605bab457204a9c17a855bacc78f052fa0c7d9c
Jasvin James <jamesjasvin5@gmail.com>
2020-04-08 22:29:40 +0530

    Handled secret key and session key error

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import shutil
 import uuid
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
+# app.secret_key = os.urandom(24)
+app.secret_key = os.environ['SECRET_KEY']
 
 # Set the upload folder for whatever we upload
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -45,102 +46,104 @@ def home(error=None):
 
 # Check if filename is an allowed_image or an allowed_video
 def allowed_image(filename):
-    return '.' in filename and filename.split('.')[-1].lower() in IMAGE_EXTENSIONS
+	return '.' in filename and filename.split('.')[-1].lower() in IMAGE_EXTENSIONS
 
 
 def allowed_video(filename):
-    return '.' in filename and filename.split('.')[-1].lower() in VIDEO_EXTENSIONS
+	return '.' in filename and filename.split('.')[-1].lower() in VIDEO_EXTENSIONS
 
 
 @app.route('/uploads', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        file_object = request.files
-        
-        for f in file_object:
-            file = request.files.get(f)
+	if request.method == 'POST':
+		file_object = request.files
+		
+		for f in file_object:
+			file = request.files.get(f)
 
-            # General good practice of security while using filenames of user uploaded files
-            # https://flask.palletsprojects.com/en/1.1.x/quickstart/#file-uploads
-            user_uuid = session['user_uuid']
-            file_uuid = str(uuid.uuid4().hex)[:5]
-            # filename = secure_filename(file.filename)
-            filename = user_uuid + file_uuid + '.' + file.filename.split('.')[-1]
+			# General good practice of security while using filenames of user uploaded files
+			# https://flask.palletsprojects.com/en/1.1.x/quickstart/#file-uploads
+			print(session)
+			if 'user_uuid' in session: 
+				user_uuid = session['user_uuid']
+				file_uuid = str(uuid.uuid4().hex)[:5]
+				# filename = secure_filename(file.filename)
+				filename = user_uuid + file_uuid + '.' + file.filename.split('.')[-1]
 
-            # If file is an allowed image or video then save it in their respective input folder
-            if allowed_image(filename):
-                file.save(os.path.join(images_input_folder_path, filename))
+				# If file is an allowed image or video then save it in their respective input folder
+				if allowed_image(filename):
+					file.save(os.path.join(images_input_folder_path, filename))
 
-            if allowed_video(filename):
-                file.save(os.path.join(videos_input_folder_path, filename))
+				if allowed_video(filename):
+					file.save(os.path.join(videos_input_folder_path, filename))
 
-        return "UPLOADING"
+		return "UPLOADING"
 
-    # If it wasn't a POST request then just redirect to the home page
-    return redirect(url_for('home'))
+	# If it wasn't a POST request then just redirect to the home page
+	return redirect(url_for('home'))
 
 
 # This route is triggered when "Display Results button is clicked on the home page
 @app.route('/results')
 def results():
 
-    # Whenever uploads are to be processed, clear the image and video output folders, in order to prevent previous
-    # outputs from being processed
-    # delete_directory_files(images_output_folder_path)
-    user_uuid = session['user_uuid']
-    
-    create_image_output(dir_path=images_input_folder_path, output_file_path=images_output_folder_path, user_uuid=user_uuid)
+	# Whenever uploads are to be processed, clear the image and video output folders, in order to prevent previous
+	# outputs from being processed
+	# delete_directory_files(images_output_folder_path)
+	user_uuid = session['user_uuid']
 
-    # Delete the images and videos input folders, in order to prevent it being processed for next request
-    # delete_directory_files(images_input_folder_path)
+	create_image_output(dir_path=images_input_folder_path, output_file_path=images_output_folder_path, user_uuid=user_uuid)
 
-    output_images = []
-    output_videos = []
+	# Delete the images and videos input folders, in order to prevent it being processed for next request
+	# delete_directory_files(images_input_folder_path)
 
-    for filename in os.listdir(images_output_folder_path):
-    	if user_uuid in filename:
-    		output_images.append(filename)
-    
-    # output_images = os.listdir(images_output_folder_path)
+	output_images = []
+	output_videos = []
 
-    # Repeat the same with videos as well
-    # delete_directory_files(videos_output_folder_path)
-    create_video_output(dir_path=videos_input_folder_path, output_file_path=videos_output_folder_path, user_uuid=user_uuid)
-    # delete_directory_files(videos_input_folder_path)
+	for filename in os.listdir(images_output_folder_path):
+		if user_uuid in filename:
+			output_images.append(filename)
+	
+	# output_images = os.listdir(images_output_folder_path)
 
-    for filename in os.listdir(videos_output_folder_path):
-    	if user_uuid in filename:
-    		output_videos.append(filename)
+	# Repeat the same with videos as well
+	# delete_directory_files(videos_output_folder_path)
+	create_video_output(dir_path=videos_input_folder_path, output_file_path=videos_output_folder_path, user_uuid=user_uuid)
+	# delete_directory_files(videos_input_folder_path)
 
-    # output_videos = os.listdir(videos_output_folder_path)
+	for filename in os.listdir(videos_output_folder_path):
+		if user_uuid in filename:
+			output_videos.append(filename)
 
-    # If no images and videos were uploaded to the dropzone, then both these lists would be empty
-    # In this case, redirect to home page and display Error Message
-    if len(output_images) == 0 and len(output_videos) == 0:
-        return redirect(url_for('home', error="NO FILE UPLOADED"))
-    
-    # Render the results page
-    return render_template('results.html', output_images=output_images, output_videos=output_videos)
+	# output_videos = os.listdir(videos_output_folder_path)
+
+	# If no images and videos were uploaded to the dropzone, then both these lists would be empty
+	# In this case, redirect to home page and display Error Message
+	if len(output_images) == 0 and len(output_videos) == 0:
+		return redirect(url_for('home', error="NO FILE UPLOADED"))
+	
+	# Render the results page
+	return render_template('results.html', output_images=output_images, output_videos=output_videos)
 
 
 # Delete all files in given directory path
 def delete_directory_files(dir_path):
-    for filename in os.listdir(dir_path):
-        file_path = os.path.join(dir_path, filename)
-        try:
-            shutil.rmtree(file_path)
-        except OSError:
-            os.remove(file_path)
+	for filename in os.listdir(dir_path):
+		file_path = os.path.join(dir_path, filename)
+		try:
+			shutil.rmtree(file_path)
+		except OSError:
+			os.remove(file_path)
 
 
 # This route is to send images to UI after resizing it properly (which cannot be done on the front-end side)
 @app.route('/results/<file_type>/<file_name>')
 def send_file(file_type, file_name):
-    if file_type == "image":
-        return send_from_directory(images_output_folder_path, file_name)
-    if file_type == "video":
-        return send_from_directory(videos_output_folder_path, file_name)
+	if file_type == "image":
+		return send_from_directory(images_output_folder_path, file_name)
+	if file_type == "video":
+		return send_from_directory(videos_output_folder_path, file_name)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
